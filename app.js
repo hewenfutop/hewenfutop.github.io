30f70bb705a1cfdc6ceb9f35d799bef7007e70b2
weiweicenxi <weiweicenxi@163.com>
2019-03-31 21:54:07 +0800

    更新

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ $$("btn-down").onmouseout=function () {
         els[i].style.display="none";
     }
 };
+
+
+
+var mdEls=document.getElementsByClassName("md-btn-download-resume");
+$$("md-btn-down").onmouseover=function () {
+    // $$("md-tips").style.display="none";
+
+    for(var i=0;i<mdEls.length;i++){
+        mdEls[i].style.display="block";
+        mdEls[i].style.textDecoration="none";
+    }
+};
+
+$$("md-btn-down").onmouseout=function () {
+    // $$("md-tips").style.display="block";
+    for(var i=0;i<mdEls.length;i++){
+        mdEls[i].style.display="none";
+    }
+};
